9633acf51868557c3b7015870f1af99aad8a698f
Paul Darragh <pdarragh45@massmutual.com>
2017-11-08 12:34:53 -0500

    working hello world component

---
+++ src/docs/examples/HelloWorld/ExampleHelloWorld.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import HelloWorld from 'component-library/HelloWorld';
+
+/** Custom Message */
+export default function ExampleHellowWorld() {
+  return <HelloWorld message="People" />
+}
